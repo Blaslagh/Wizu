5eb483aa53f99fb65817261b683942eba91ea8c7
Adam Iwanicki <iwanicki.a.m@gmail.com>
2020-01-06 22:36:57 +0100

    Generowanie chmur do n-tego miejsca

--- chmurkowanko2.py
+++ chmurkowanko2.py
@@ -19,6 +19,21 @@ def chmuruj_slownik(wejscie_slownik=przetwarzanko.wczytywanko("Dane.txt"), sciez
 	plt.savefig(sciezka)
 	return
 
+def chmuruj_slownik_ponad_x(wejscie_slownik=przetwarzanko.wczytywanko("Dane.txt"), sciezka='chmura.jpg', x = 2):
+
+	slownik_do_chmury = {}
+
+	for i in wejscie_slownik.keys():
+		if len(str(i)) > x:
+			slownik_do_chmury[ str(i) ] = float( wejscie_slownik[i] )
+
+	wordcloud = WordCloud().generate_from_frequencies(slownik_do_chmury)
+
+	plt.imshow(wordcloud, interpolation="bilinear")
+	plt.axis("off")
+	plt.savefig(sciezka)
+	return
+
 def chmury_4_all(sciezka):
 	if not os.path.exists(sciezka):
 		print("Ni ma")
@@ -33,24 +48,9 @@ def chmury_4_all(sciezka):
 
 		for rok in [x for x in os.listdir(sciezka+'\\'+artysta) if os.path.isdir(sciezka+'\\'+artysta+'\\'+x)]:
 			try:
-				chmuruj_slownik(przetwarzanko.wczytywanko(sciezka+'\\'+artysta+'\\'+rok+'.txt'),"Chmury\\"+artysta+'\\'+rok+'.jpg')
+				chmuruj_slownik_ponad_x(przetwarzanko.wczytywanko(sciezka+'\\'+artysta+'\\'+rok+'.txt'),"Chmury\\"+artysta+'\\'+rok+'.jpg')
 				print( "o", end = '' )
 			except:
 				print( "N", end = '' )
-	return
-
-def chmuruj_slownik_ponad_4(wejscie_slownik=przetwarzanko.wczytywanko("Dane.txt"), sciezka='chmura.jpg'):
-
-	slownik_do_chmury = {}
-
-	for i in wejscie_slownik.keys():
-		if len(str(i)) > 3:
-			slownik_do_chmury[ str(i) ] = float( wejscie_slownik[i] )
-
-	wordcloud = WordCloud().generate_from_frequencies(slownik_do_chmury)
-
-	plt.imshow(wordcloud, interpolation="bilinear")
-	plt.axis("off")
-	plt.savefig(sciezka)
 	return
 
